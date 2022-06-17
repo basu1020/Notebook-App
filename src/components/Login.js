530732cb60d6973c33d032d2b746b2f4cdb62e48
basu1020 <bvp1020mu@gmail.com>
2022-06-17 10:19:44 +0530

    17-06-2022 -2

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import authContext from '../context/auth/authContext'
 
@@ -6,24 +6,37 @@ const Login = () => {
     const context = useContext(authContext)
     const { login, setLoggedIN } = context
     let navigate = useNavigate()
+    const [loginQuery, setLoginQuery] = useState({email : "", password: ""})
+
+    useEffect(() => {
+        if(localStorage.getItem("token")){
+            navigate("/user")
+        }
+        else{
+            navigate("/")
+        }
+        // eslint-disable-next-line
+    }, [])
 
     const handleClick = async (e) => {
         e.preventDefault()
-        const inputEmail = document.querySelector('#inputEmail')
-        const inputPassword = document.querySelector('#inputPassword')
         try {
-            const json = await login(inputEmail.value, inputPassword.value)
-            if(json){
+            const json = await login(loginQuery.email, loginQuery.password)
+            if(json.authToken){
                 await setLoggedIN(true)
                 localStorage.setItem("token", json.authToken)
                 navigate("/user")
             }
             else{
-                alert("error")
+                alert(json.error)
             }
         } catch (error) {
             alert(error)
         }
+    }
+
+    const onChange = (e) => {
+        setLoginQuery({...loginQuery, [e.target.name]: e.target.value})
     }
 
     return (
@@ -34,13 +47,13 @@ const Login = () => {
                     <div className="row mb-3">
                         <label htmlFor="inputEmail3" className="col-sm-2 col-form-label"> Email </label>
                         <div className="col-sm-10">
-                            <input type="email" className="form-control" id="inputEmail" />
+                            <input type="email" className="form-control" name="email" value={loginQuery.email} id="inputEmail" onChange={onChange}/>
                         </div>
                     </div>
                     <div className="row mb-3">
                         <label htmlFor="inputPassword3" className="col-sm-2 col-form-label"> Password </label>
                         <div className="col-sm-10">
-                            <input type="password" className="form-control" id="inputPassword" />
+                            <input type="password" className="form-control" name="password" value={loginQuery.password} id="inputPassword" onChange={onChange}/>
                         </div>
                     </div>
                     <button className="btn btn-primary" onClick={handleClick}> Log in </button>
