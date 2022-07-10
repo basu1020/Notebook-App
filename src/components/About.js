27b858023e31fb1206f19a1a61c4e7f8f485ca78
basu1020 <bvp1020mu@gmail.com>
2022-07-10 18:53:59 +0530

    refactored about.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,13 +5,13 @@ import React from 'react'
 const About = () => {
   return (
     <>
-      <h1>This is a complete MERN stack project</h1>
-      <h1>I have used Node.js for Backend</h1>
-      <h1>I have used Express.js for making APIs</h1>
-      <h1>MongoDb is used as a database</h1>
-      <h1>And Finally I have used REACT.js for Frontend portion</h1>
-      <h1>You can view the code in here   </h1>
-      <a href="https://github.com/basu1020">
+      <h1>This is a complete MERN stack project.</h1>
+      <h1>I have used Node.js for Backend.</h1>
+      <h1>I have used Express.js for making APIs.</h1>
+      <h1>MongoDb is used as a database.</h1>
+      <h1>And Finally I have used REACT.js for Frontend portion.</h1>
+      <span>You can view the code in here.</span>
+      <a href="https://github.com/basu1020/Notebook-App">
         <i class="fa-brands fa-github" ></i>
       </a>
     </>
