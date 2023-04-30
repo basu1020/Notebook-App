76419a8eaa5dfb4359b9c71ce4da25566e500855
basu1020 <bvp1020mu@gmail.com>
2023-04-30 09:43:50 +0530

    changed port

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ const connectToMongo = require("./db")
 connectToMongo()
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 const cors = require('cors')
 
 app.use(cors())
